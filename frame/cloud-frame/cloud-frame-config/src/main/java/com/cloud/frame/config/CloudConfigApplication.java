4c0a734aebf096cb4a79ca69163f0f5114fc0341
wd <wd@weidaideMacBook-Pro.local>
2018-03-23 10:36:04 +0800

    config test

---
+++ frame/cloud-frame/cloud-frame-config/src/main/java/com/cloud/frame/config/CloudConfigApplication.java
@@ -0,0 +1,21 @@
+package com.cloud.frame.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableConfigServer
+//@EnableConfigServer
+//@EnableDiscoveryClient
+//@SpringBootApplication
+public class CloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigApplication.class, args);
+    }
+}
