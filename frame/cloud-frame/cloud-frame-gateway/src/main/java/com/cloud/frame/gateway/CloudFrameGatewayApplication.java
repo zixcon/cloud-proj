d1d817035c8d1acf0089dd58899b2bbf90f01786
wd <wd@weidaideMacBook-Pro.local>
2018-03-29 19:57:24 +0800

    zuul配置

--- frame/cloud-frame/cloud-frame-gateway/src/main/java/com/cloud/frame/gateway/CloudFrameGatewayApplication.java
+++ frame/cloud-frame/cloud-frame-gateway/src/main/java/com/cloud/frame/gateway/CloudFrameGatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -13,5 +15,24 @@ public class CloudFrameGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudFrameGatewayApplication.class, args);
     }
+
+//    @Bean
+//    public PatternServiceRouteMapper serviceRouteMapper(){
+//
+//        // servicePattern: 指的是微服务的pattern
+//
+//        // routePattern: 指的是路由的pattern
+//
+//        // 当你访问/microservice-provider-user/v1 其实就是
+//
+//        // localhost:8040/v1/microservice-provider-user/user/1
+//
+//        return new PatternServiceRouteMapper(
+//
+//                "(?<name>^.+)-(?<version>v.+$)","${version}/${name}"
+//
+//        );
+//
+//    }
 
 }
