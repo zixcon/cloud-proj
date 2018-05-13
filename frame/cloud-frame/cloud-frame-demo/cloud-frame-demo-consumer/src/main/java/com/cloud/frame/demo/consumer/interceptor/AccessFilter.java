a277b02c140457a5332f99a10e4bdc1acc4f6a04
wd <wd@weidaideMacBook-Pro.local>
2018-05-13 19:52:57 +0800

    zuul

---
+++ frame/cloud-frame/cloud-frame-demo/cloud-frame-demo-consumer/src/main/java/com/cloud/frame/demo/consumer/interceptor/AccessFilter.java
@@ -0,0 +1,25 @@
+package com.cloud.frame.demo.consumer.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by wd on 2018/5/11.
+ */
+@Component
+@Slf4j
+public class AccessFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("请求地址：{}", request.getRequestURI());
+        filterChain.doFilter(request, response);
+    }
+}
